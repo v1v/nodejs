74fec4e91987aab15bb5234cefc51275e53ad2e3
Shawn Neal <sneal@sneal.net>
2013-11-15 16:39:20 -0800

    Minitests work for both Linux and Windows

--- files/default/tests/minitest/default_test.rb
+++ files/default/tests/minitest/default_test.rb
@@ -4,12 +4,11 @@ require 'mixlib/shellout'
 class TestNodeJsInstall < MiniTest::Chef::Spec
   describe 'node version' do
     it 'installs the specified version' do
-      if windows? then
-        nodejs_cmd = Mixlib::ShellOut.new('C:\"Program Files (x86)"\nodejs\node.exe --version')
-        nodejs_cmd.run_command
-        result = nodejs_cmd.stdout.chomp
-        assert_includes result, node['nodejs']['version']
-      end
+      cmd = windows? ? 'C:\"Program Files (x86)"\nodejs\node.exe --version' : 'node --version'
+      nodejs_cmd = Mixlib::ShellOut.new(cmd)
+      nodejs_cmd.run_command
+      result = nodejs_cmd.stdout.chomp
+      assert_includes result, node['nodejs']['version']
     end
   end
 end
