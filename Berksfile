88017eace1c1052d96961212c1db6321422bb8c5
Shawn Neal <sneal@sneal.net>
2013-11-15 16:46:47 -0800

    Added missing yum cookbook dependency

--- Berksfile
+++ Berksfile
@@ -1,6 +1,7 @@
 site :opscode
 metadata
 
+cookbook 'yum'
 cookbook 'apt', '~> 1.7.0'
 cookbook 'build-essential'
 cookbook "windows", github: "opscode-cookbooks/windows", tag: "v1.11.0"
--- metadata.rb
+++ metadata.rb
@@ -8,14 +8,15 @@ name             'nodejs'
 provides         'nodejs'
 
 recipe 'nodejs', 'Installs Node.JS based on the default installation method'
+recipe 'nodejs::windows', 'Installs Node.JS on Windows using the standard installer'
 recipe 'nodejs::install_from_source', 'Installs Node.JS from source'
 recipe 'nodejs::install_from_binary', 'Installs Node.JS from official binaries'
 recipe 'nodejs::install_from_package', 'Installs Node.JS from packages; supports windows'
 recipe 'nodejs::npm', 'Installs npm from source - a package manager for node'
 
-depends 'build-essential'
-depends 'apt'
-depends 'windows'
+%w{ apt yum build-essential windows }.each do |c|
+  depends c
+end
 
 %w{ debian ubuntu centos redhat smartos windows }.each do |os|
   supports os
