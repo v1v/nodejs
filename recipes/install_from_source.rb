71bb9d05749bd823daf62630a35746baaf4aa54b
Shawn Neal <sneal@sneal.net>
2013-11-15 17:40:46 -0800

    Fixed linting error in source recipe, without breaking the recipe

--- recipes/install_from_source.rb
+++ recipes/install_from_source.rb
@@ -55,7 +55,7 @@ bash "compile node.js (on #{node['nodejs']['make_threads']} cpu)" do
   # OSX doesn't have the attribute so arbitrarily default 2
   cwd "/usr/local/src/node-v#{node['nodejs']['version']}"
   code <<-EOH
-    PATH="/usr/local/bin:$PATH"
+    PATH=/usr/local/bin:$PATH
     ./configure --prefix=#{node['nodejs']['dir']} && \
     make -j #{node['nodejs']['make_threads']}
   EOH
