05e467e4b938df16cb89bbb87ac75333526a7c54
Shawn Neal <sneal@sneal.net>
2014-01-29 16:11:40 -0800

    Read cookbook version from version.txt

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,8 @@ maintainer_email 'marius@promethost.com'
 license          'Apache 2.0'
 description      'Installs/Configures nodejs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version (ENV['BUILD_NUMBER'] ? "1.3.#{ENV['BUILD_NUMBER']}" : '1.3.0')
+ver_path = File.join(File.dirname(__FILE__), 'version.txt')
+version ((IO.read(ver_path) if File.exists?(ver_path)) || '0.0.1').chomp
 name             'nodejs'
 provides         'nodejs'
 
