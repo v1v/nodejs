4f0d7970abc032dd2711b3d36e4a1e4a00a1b774
Shawn Neal <sneal@sneal.net>
2014-02-28 14:42:50 -0800

    Upgrade default NodeJS version to 10.26

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,13 +18,13 @@
 #
 
 default['nodejs']['install_method'] = 'source'
-default['nodejs']['version'] = '0.10.22'
+default['nodejs']['version'] = '0.10.26'
 default['nodejs']['checksum'] =
-  '7e2079394efe82f62798178f617888c9d6a39150c76122c432ae9ea73ce28e79'
+  'ef5e4ea6f2689ed7f781355012b942a2347e0299da0804a58de8e6281c4b1daa'
 default['nodejs']['checksum_linux_x64'] =
-  'cc7ccfce24ae0ebb0c50661ef8d98b5db07fc1cd4a222c5d1ae232260d5834ca'
+  '305bf2983c65edea6dd2c9f3669b956251af03523d31cf0a0471504fd5920aac'
 default['nodejs']['checksum_linux_x86'] =
-  'e8c75ffa508bb86b6858855d33139d63013c55b398f2258b020a84d8071b13f3'
+  '8fa2d952556c8b5aa37c077e2735c972c522510facaa4df76d4244be88f4dc0f'
 default['nodejs']['dir'] = '/usr/local'
 default['nodejs']['npm'] = '1.2.14'
 default['nodejs']['src_url'] = 'http://nodejs.org/dist'
