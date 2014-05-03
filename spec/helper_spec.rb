e22fff6218fa2ce4e9e7ac04016f90575d05ff16
Shawn Neal <sneal@sneal.net>
2014-05-03 13:13:03 -0700

    Added linux version test

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -2,25 +2,42 @@ require 'spec_helper'
 require_relative '../libraries/helper'
 
 describe NodejsCookbook::Helper do
-
   let(:shellout) { double('shellout') }
-
-  let(:node) do
-    { 'nodejs' => { 'bin_dir' => ::File.join('c:', 'Program Files', 'nodejs') },
-      'platform' => 'windows' }
-  end
-
   let(:helper) { NodejsCookbook::Helper.new(node) }
 
-  it 'should check installed nodejs version' do
-    node_path = 'c:/Program Files/nodejs/node.exe'
-    Mixlib::ShellOut.should_receive(:new).with("\"#{node_path}\" --version").
-      and_return(shellout)
-    shellout.should_receive(:run_command)
-    File.stub(:exists?).with(node_path).and_return(true)
-    shellout.stub(:stdout).and_return("v0.10.15\n")
+  describe 'windows' do
+    let(:node) do
+      { 'nodejs' => { 'bin_dir' => ::File.join('c:', 'Program Files', 'nodejs') },
+        'platform' => 'windows' }
+    end
 
-    expect(helper.installed_version()).to eq('v0.10.15')
+    it 'should check installed nodejs version' do
+      node_path = 'c:/Program Files/nodejs/node.exe'
+      Mixlib::ShellOut.should_receive(:new).with("\"#{node_path}\" --version").
+        and_return(shellout)
+      shellout.should_receive(:run_command)
+      File.stub(:exists?).with(node_path).and_return(true)
+      shellout.stub(:stdout).and_return("v0.10.15\n")
+
+      expect(helper.installed_version()).to eq('v0.10.15')
+    end
   end
 
+  describe 'linux' do
+    let(:node) do
+      { 'nodejs' => { 'bin_dir' => '/usr/bin' },
+        'platform' => 'ubuntu' }
+    end
+
+    it 'should check installed nodejs version' do
+      node_path = '/usr/bin/node'
+      Mixlib::ShellOut.should_receive(:new).with("\"#{node_path}\" --version").
+        and_return(shellout)
+      shellout.should_receive(:run_command)
+      File.stub(:exists?).with(node_path).and_return(true)
+      shellout.stub(:stdout).and_return("v0.10.15\n")
+
+      expect(helper.installed_version()).to eq('v0.10.15')
+    end
+  end
 end
